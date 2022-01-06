e879cd334a11bb4f09064cbf8b072270fedbe62c
Sean <zhuqingyun@outlook.com>
2022-01-06 14:03:23 -0700

    Use public folder and move scripts and styles folder into it

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('public'));  // midware, move scripts and styles folder to the public folder
+
 app.get('/', function (req, res) {
     res.send('<h1>Hello World!</h1>');
 });
